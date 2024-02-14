31262f5352ed1ad389185bb470fc85fb468df478
Luan Flavio <luanflavio.bm@gmail.com>
2024-02-14 11:49:05 -0300

    [feat] Added gif images for projects

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 const projetos = [{
     "titulo": "Calculadora Estatistica",
     "texto": "Programa feito em Python, para calculo de Pareto, Histograma, Cálculo Binomial com CRUD.",
-    "imagem": "./src/assets/images/image.png",
+    "imagem": "./src/assets/projects-images/ProjetoEST.png",
     "alternativo": "Teste 1",
     "href" : "https://github.com/Luan-FlavioBr/Projeto-EST"
 },
 {
-    "titulo": "Calculadora Estatistica",
-    "texto": "Programa feito em Python, para calculo de Pareto, Histograma, Cálculo Binomial com CRUD.",
-    "imagem": "./src/assets/images/image.png",
+    "titulo": "Sistema de PetShop",
+    "texto": "Programa feito em Java, para cadastro de pacientes em PetShop, com CRUD",
+    "imagem": "./src/assets/projects-images/SistemaPet.gif",
     "alternativo": "Teste 1",
     "href" : "https://github.com/Luan-FlavioBr/Projeto-EST"
 },
 {
-    "titulo": "Calculadora Estatistica",
-    "texto": "Programa feito em Python, para calculo de Pareto, Histograma, Cálculo Binomial com CRUD.",
-    "imagem": "./src/assets/images/image.png",
+    "titulo": "Conversor de Medidas",
+    "texto": "Programa feito em Java, para conversão de moedas, massas e temperatura.",
+    "imagem": "./src/assets/projects-images/Conversor.gif",
     "alternativo": "Teste 1",
     "href" : "https://github.com/Luan-FlavioBr/Projeto-EST"
 },
 {
-    "titulo": "Calculadora Estatistica",
-    "texto": "Programa feito em Python, para calculo de Pareto, Histograma, Cálculo Binomial com CRUD.",
-    "imagem": "./src/assets/images/image.png",
+    "titulo": "Codificador/Decodificador",
+    "texto": "Programa feito em HTML, CSS e JS, para codificar/decodificar textos, para o desafio alura.",
+    "imagem": "./src/assets/projects-images/Codificador.gif",
+    "alternativo": "Teste 1",
+    "href" : "https://github.com/Luan-FlavioBr/Projeto-EST"
+},
+{   "titulo": "Calculadora de Sistemas",
+    "texto": "Programa feito em Python, para calcular sistemas 2x2 e 3x3, podendo fazer passa a passo.",
+    "imagem": "./src/assets/projects-images/SistemasCalc.gif",
     "alternativo": "Teste 1",
     "href" : "https://github.com/Luan-FlavioBr/Projeto-EST"
 }];
