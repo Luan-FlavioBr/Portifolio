a7459776d65a9f549b8cc3c5917d16200716f824
Luan Flavio <luanflavio.bm@gmail.com>
2024-02-14 12:08:52 -0300

    [Feat] Added links to projects and alt text

--- script.js
+++ script.js
@@ -2,41 +2,41 @@ const projetos = [{
     "titulo": "Calculadora Estatistica",
     "texto": "Programa feito em Python, para calculo de Pareto, Histograma, Cálculo Binomial com CRUD.",
     "imagem": "./src/assets/projects-images/ProjetoEST.png",
-    "alternativo": "Teste 1",
+    "alternativo": "PNG da tela inicial da calculadora estatística.",
     "href" : "https://github.com/Luan-FlavioBr/Projeto-EST"
 },
 {
     "titulo": "Sistema de PetShop",
     "texto": "Programa feito em Java, para cadastro de pacientes em PetShop, com CRUD",
     "imagem": "./src/assets/projects-images/SistemaPet.gif",
-    "alternativo": "Teste 1",
-    "href" : "https://github.com/Luan-FlavioBr/Projeto-EST"
+    "alternativo": "GIF do sistema funcionando.",
+    "href" : "https://github.com/Luan-FlavioBr/SistemaPetShopFATEC"
 },
 {
     "titulo": "Conversor de Medidas",
     "texto": "Programa feito em Java, para conversão de moedas, massas e temperatura.",
     "imagem": "./src/assets/projects-images/Conversor.gif",
-    "alternativo": "Teste 1",
-    "href" : "https://github.com/Luan-FlavioBr/Projeto-EST"
+    "alternativo": "GIF do conversor funcionando.",
+    "href" : "https://github.com/Luan-FlavioBr/Conversor-alura-challenge"
 },
 {
     "titulo": "Codificador/Decodificador",
     "texto": "Programa feito em HTML, CSS e JS, para codificar/decodificar textos, para o desafio alura.",
     "imagem": "./src/assets/projects-images/Codificador.gif",
-    "alternativo": "Teste 1",
-    "href" : "https://github.com/Luan-FlavioBr/Projeto-EST"
+    "alternativo": "GIF do Codificador/Decodificador funcionando.",
+    "href" : "https://github.com/Luan-FlavioBr/Challenge-Oracle-ONE"
 },
 {   "titulo": "Calculadora de Sistemas",
     "texto": "Programa feito em Python, para calcular sistemas 2x2 e 3x3, podendo fazer passa a passo.",
     "imagem": "./src/assets/projects-images/SistemasCalc.gif",
-    "alternativo": "Teste 1",
-    "href" : "https://github.com/Luan-FlavioBr/Projeto-EST"
+    "alternativo": "GIF da calculadora funcionando no Terminal do VSCode.",
+    "href" : "https://github.com/Luan-FlavioBr/Calculadora-de-Sistemas"
 },
 {   "titulo": "Parallax Site",
     "texto": "Site com estilo Parallax, feito em HTML e CSS.",
     "imagem": "./src/assets/projects-images/Parallax.gif",
-    "alternativo": "Teste 1",
-    "href" : "https://github.com/Luan-FlavioBr/Projeto-EST"
+    "alternativo": "GIF do site Parallax funcionando.",
+    "href" : "https://github.com/Luan-FlavioBr/Parallax"
 }];
 
 const cardProjetos = document.querySelector('#projetos');
